60b652d0cff13ac23b4c03df14973052b446544a
Chloemlla <happyclovo@gmail.com>
2026-04-30 21:00:18 +0800

    fix: update Tailwind CSS configuration and import scrollbar plugin

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,5 @@
+import tailwindScrollbar from "tailwind-scrollbar";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -37,7 +39,5 @@ export default {
       }
     },
   },
-  plugins: [
-    require('tailwind-scrollbar'),
-  ],
+  plugins: [tailwindScrollbar],
 }
